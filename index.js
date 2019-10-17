60a91993ed7ca94264309becba607a618b2ab9f0
Troy Whiteley <24781+dawnerd@users.noreply.github.com>
2019-10-17 16:40:18 +0000

    trying this?

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 module.exports = function (arc, cloudformation, stage) {
   // @TODO: Read arc config for values
   cloudformation.Resources.StaticBucket.Properties.CorsConfiguration = {
-    CORSRules: {
-      AllowedHeaders: ['Authorization'],
-      AllowedMethods: ['GET', 'HEAD'],
-      AllowedOrigins: ['*'],
-      MaxAgeSeconds: 3000
-    }
+    CORSRules: [
+      { AllowedHeaders: ['Authorization'] },
+      { AllowedMethods: ['GET', 'HEAD'] },
+      { AllowedOrigins: ['*'] },
+      { MaxAgeSeconds: 3000 }
+    ]
   };
 
   return cloudformation;
